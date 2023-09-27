351b0fc1de36a732e99f229699ca8a62e116fc3a
Samuel Araag <samuelaraag@Samuels-MacBook-Air.local>
2023-09-27 20:25:36 -0300

    tabuada

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
-print("Tabuada")
+print("TABUADA")
+num = int(input("Digite um número e dê enter para ver a tabuada5: "))
 
-num = int(input("Digite um número entre 1 e 10: "))
+if num:
+    print("A tabuada do número {} é: ".format(num))
+    base = 11
+    i = 1
 
-print("A tabuada do número {} é: ".format(num))
-
-base = 11
-i = 1
-
-while i < base:
-    print("{} x {} é igual a {}".format(num, i, num*i))
-    i += 1
+    while i < base:
+        print("{} x {} é igual a {}".format(num, i, num*i))
+        i += 1
 
 print("Obrigado por acessar a nossa calculadora.")
-###
-num = int(input("Digite um número: "))
-prox = num + 1
-anterior = num + -1
-
-print("O número que você digitou foi: {}. O próximo número é {} e o anterior é {}".format(num, prox, anterior))
-
-nota1 = int(input("Digite a primeira nota do aluno: "))
-nota2 = int(input("Digite a segunda nota do aluno: "))
-
-print("A média desse aluno foi: {}".format((nota1+nota2)/2))
