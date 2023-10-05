c1ebcd549ba74fcbb36b48286ebcb6d4c60df423
Samuel Santos Jesus <samuel.santos@inventsoftware.com.br>
2023-10-05 13:33:13 -0300

    obter nome randomico

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-##extrair numero inteiro
+##obter um nome aleatorio a partir de uma lista
+import random
 
-num =  round(float(input("Digite um número: ")))
+n1 = input("Digite um nome:")
+n2 = input("Digite outro nome:")
+n3 = input("Digite outro nome:")
+n4 = input("Digite outro nome:")
+n5 = input("Digite outro nome:")
 
-print(num)
+nomes = [n1, n2, n3, n4, n5]
+
+nomeEscolhido = random.choice(nomes)
+
+print(nomeEscolhido)
