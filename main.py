d66c0b087b7e15444af137dd5096c6f0d680bb34
Samuel Santos Jesus <samuel.santos@inventsoftware.com.br>
2023-10-05 12:32:44 -0300

    numero inteiro

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-##importando bibliotecas
+##extrair numero inteiro
 
-##importa tudo, import math import sqrt [somente esse metodo], import math import sqrt, pow [importa varios metodos]
-import math
+num =  round(float(input("Digite um número: ")))
 
-num = int(input("Digite um número: "))
-
-raiz = math.sqrt(num)
-
-print("A raiz quadrada de {} é {}".format(num, math.ceil(math.sqrt(raiz))))
+print(num)
